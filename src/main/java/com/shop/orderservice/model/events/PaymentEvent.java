e7d82c72f1f1122ecf61c59e514925d1f5f1cec9
Vyachelav Paltsev <fate6002@gmail.com>
2026-03-14 23:53:22 +0300

    ORD-9: create payment event, refactor OrderStatus, refactor applciation.yaml

---
+++ src/main/java/com/shop/orderservice/model/events/PaymentEvent.java
@@ -0,0 +1,17 @@
+package com.shop.orderservice.model.events;
+
+import com.shop.orderservice.model.entities.PaymentStatus;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class PaymentEvent {
+
+    private Long orderId;
+    private Long userId;
+    private PaymentStatus status;
+
+}
